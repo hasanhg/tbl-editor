6805f27eebf7aad3a4187824daeb377051a2cb3d
Hasan Gürsoy <hasan@robomotion.io>
2021-11-13 20:21:51 +0300

    filtering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React from 'react';
+import { withStyles } from '@mui/styles';
 import ReactDataSheet from 'react-datasheet';
 import 'react-datasheet/lib/react-datasheet.css';
-import { IconButton, Input, SvgIcon, TextField, Tooltip, Typography } from '@mui/material';
-import { withStyles } from '@mui/styles';
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, FormGroup, FormLabel, IconButton, Input, Radio, RadioGroup, SvgIcon, TextField, Tooltip, Typography } from '@mui/material';
 import TBLBuffer from './tbl/buffer';
 import { TypeTitles } from './tbl/const';
-import { mdiArrowLeft, mdiArrowRight, mdiContentSave, mdiFolderOpen, mdiMenuOpen, mdiOpenid, mdiOpenInNew } from '@mdi/js';
+import { mdiArrowLeft, mdiArrowRight, mdiContentSave, mdiFilter, mdiFilterOutline, mdiFolderOpen } from '@mdi/js';
 
 
 const styles = theme => ({
@@ -26,6 +26,9 @@ const styles = theme => ({
   },
   pageInput: {
     textAlign: 'center',
+  },
+  filterInput: {
+
   }
 });
 
@@ -41,6 +44,16 @@ class App extends React.Component {
       page: 0,
       _page: 0,
       size: 100,
+
+      // Filters properties
+      filtering: false,
+      filterStr: '',
+      //findDirection: 'all',
+      filterInCol: null,
+      filterMatchCase: false,
+      filters: {},
+      filteredGrid: [],
+
     };
 
     this.inputRef = React.createRef();
@@ -89,11 +102,11 @@ class App extends React.Component {
     const { columns, selections } = this.state
     switch (this.state.as) {
       case 'list':
-        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='segment' headerAs='div' bodyAs='ul' rowAs='div' cellAs='div' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} />
+        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='segment' headerAs='div' bodyAs='ul' rowAs='div' cellAs='div' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} doFilter={this.doFilter} />
       case 'div':
-        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='div' headerAs='div' bodyAs='div' rowAs='div' cellAs='div' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} />
+        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='div' headerAs='div' bodyAs='div' rowAs='div' cellAs='div' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} doFilter={this.doFilter} />
       default:
-        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='table' headerAs='thead' bodyAs='tbody' rowAs='tr' cellAs='th' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} />
+        return <SheetRenderer columns={columns} selections={selections} onSelectAllChanged={this.handleSelectAllChanged} as='table' headerAs='thead' bodyAs='tbody' rowAs='tr' cellAs='th' {...props} {...this.props} setState={s => this.setState(s)} state={this.state} doFilter={this.doFilter} />
     }
   }
 
@@ -145,8 +158,11 @@ class App extends React.Component {
       }
     }
 
+    if (!file) return;
+
     const fr = new FileReader();
     fr.onload = (e) => {
+      if (!e.target.result) return;
       const b64 = e.target.result;
       const buf = Buffer.from(b64);
       const tbl = new TBLBuffer(buf);
@@ -168,19 +184,20 @@ class App extends React.Component {
         });
       });
 
-      this.setState({ _grid, columns, grid: _grid.slice(0, this.state.size), page: 0, _page: 1 });
+      const grid = _grid.slice(0, this.state.size);
+      this.setState({ _grid, columns, grid, filteredGrid: _grid, page: 0, _page: 1 });
     };
 
     fr.readAsArrayBuffer(file);
   }
 
   refreshTable = (page, size) => {
-    const total = this.state._grid.length / size;
+    const total = this.state.filteredGrid.length / size;
     page = Math.max(page, 0);
     page = Math.min(page, total);
     const min = page * size;
     const max = min + size;
-    this.setState({ grid: this.state._grid.slice(min, max), page, size, _page: page + 1 });
+    this.setState({ grid: this.state.filteredGrid.slice(min, max), page, size, _page: page + 1 });
   }
 
   onPrevPage = () => {
@@ -198,7 +215,7 @@ class App extends React.Component {
 
   pagination = () => {
     const { classes } = this.props;
-    const total = Math.ceil(this.state._grid.length / this.state.size);
+    const total = Math.ceil(this.state.filteredGrid.length / this.state.size);
     const page = Math.min(this.state.page + 1, total);
     const disabled = total === 0;
 
@@ -209,9 +226,11 @@ class App extends React.Component {
         </Typography>
 
         <Tooltip title='Previous Page' placement='bottom'>
-          <IconButton onClick={this.onPrevPage} size='small' style={{ margin: '0px 4px' }} disabled={page <= 1}>
-            <SvgIcon fontSize='small'><path d={mdiArrowLeft} color={page > 1 ? '#1b93d0' : '#ccc'} /></SvgIcon>
-          </IconButton>
+          <span>
+            <IconButton onClick={this.onPrevPage} size='small' style={{ margin: '0px 4px' }} disabled={page <= 1}>
+              <SvgIcon fontSize='small'><path d={mdiArrowLeft} color={page > 1 ? '#1b93d0' : '#ccc'} /></SvgIcon>
+            </IconButton>
+          </span>
         </Tooltip>
 
         <TextField margin='dense' label='Page' variant='outlined' value={this.state._page}
@@ -231,11 +250,13 @@ class App extends React.Component {
           }} />
 
         <Tooltip title='Next Page' placement='top'>
-          <IconButton onClick={this.onNextPage} size='small' style={{ margin: '0px 4px' }} disabled={page === total}>
-            <SvgIcon fontSize='small'><path d={mdiArrowRight} color={page < total ? '#1b93d0' : '#ccc'} /></SvgIcon>
-          </IconButton>
+          <span>
+            <IconButton onClick={this.onNextPage} size='small' style={{ margin: '0px 4px' }} disabled={page === total}>
+              <SvgIcon fontSize='small'><path d={mdiArrowRight} color={page < total ? '#1b93d0' : '#ccc'} /></SvgIcon>
+            </IconButton>
+          </span>
         </Tooltip>
-      </div>
+      </div >
     );
   }
 
@@ -247,9 +268,127 @@ class App extends React.Component {
 
   }
 
+  onFilterCancel = () => {
+    this.setState({ filtering: false });
+  }
+
+  doFilter = (filters) => {
+    let grid = Array.from(this.state._grid);
+
+    Object.values(filters).forEach(filter => {
+      const rows = [];
+      grid.forEach((row, i) => {
+        let cell = filter.col ? row[filter.col] : null;
+        const found = !filter.col ? row.some(c => {
+          cell = c;
+          return filter.match ? cell.value.toString() === filter.str : cell.value.toString().includes(filter.str);
+        }) :
+          filter.match ? cell.value.toString() === filter.str : cell.value.toString().includes(filter.str);
+
+        if (found) {
+          cell.found = true;
+          row.row = i;
+          rows.push(row);
+        }
+      });
+
+      grid = Array.from(rows);
+    });
+
+    this.setState({ filteredGrid: grid, filters }, () => {
+      this.refreshTable(0, this.state.size);
+    });
+  }
+
+  onFilter = () => {
+    const { filterStr, filters, filterMatchCase } = this.state;
+    if (!filterStr) return;
+
+    filters[this.state.filterInCol] = { str: filterStr, col: this.state.filterInCol, match: filterMatchCase };
+    this.doFilter(filters);
+
+    this.setState({ filterStr: '', filterInCol: null, filterMatchCase: false });
+    this.onFilterCancel();
+  }
+
+  filterWidget = () => {
+    const { classes } = this.props;
+
+    return (
+      <Dialog
+        onClose={this.onFilterCancel}
+        open={this.state.filtering}
+        onKeyPress={(ev) => {
+          if (ev.key === 'Enter') {
+            ev.preventDefault();
+          }
+        }}
+        fullWidth={true}
+        maxWidth={"sm"}
+        disableEscapeKeyDown={false}>
+        <DialogTitle style={{ userSelect: 'none' }}>
+          {`Filter${Boolean(this.state.filterInCol) || this.state.filterInCol === 0 ? ` in ${colName(this.state.filterInCol + 1)}` : ''}`}
+        </DialogTitle>
+        <DialogContent>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography style={{ marginRight: 12, userSelect: 'none' }}>Find what:</Typography>
+            <TextField margin='dense' variant='outlined' value={this.state.filterStr}
+              onChange={e => this.setState({ filterStr: e.target.value })}
+              fullWidth
+              style={{ width: 344 }}
+              InputProps={{
+                classes: {
+                  root: classes.pageRoot,
+                  input: classes.filterInput,
+                }
+              }}
+              onKeyPress={(ev) => {
+                if (ev.key === 'Enter') {
+                  this.onFilter();
+                }
+              }} />
+          </div>
+
+          <FormControl component="fieldset" className={classes.formControl} style={{ border: '1px solid #ccc', padding: '4px 16px', marginTop: 12 }}>
+            <FormLabel component="legend" style={{ padding: '0px 8px', userSelect: 'none' }}>Options</FormLabel>
+            <FormGroup aria-label="privacy" name="privacy" value={this.state.filterMatchCase}>
+              <FormControlLabel value="match"
+                control={<Checkbox checked={this.state.filterMatchCase} onChange={e => this.setState({ filterMatchCase: !this.state.filterMatchCase })} />}
+                label={<span style={{ userSelect: 'none' }}>Match Case</span>}
+              />
+            </FormGroup>
+          </FormControl>
+
+          {
+            /*
+            <FormControl component="fieldset" className={classes.formControl} style={{ border: '1px solid #ccc', padding: '4px 16px', marginTop: 12, marginLeft: 8 }}>
+              <FormLabel component="legend" style={{ padding: '0px 8px', userSelect: 'none' }}>Direction</FormLabel>
+              <RadioGroup row aria-label="privacy" name="privacy" value={this.state.finDirection} onChange={e => { this.setState({ findDirection: e.target.value }) }} >
+                <FormControlLabel value="all" control={<Radio />} label={<span style={{ userSelect: 'none' }}>All</span>} />
+                <FormControlLabel value="up" control={<Radio />} label={<span style={{ userSelect: 'none' }}>Up</span>} />
+                <FormControlLabel value="down" control={<Radio />} label={<span style={{ userSelect: 'none' }}>Down</span>} />
+              </RadioGroup>
+            </FormControl>
+            */
+          }
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.onFilterCancel} color="error" variant="contained">
+            Cancel
+          </Button>
+          <Button onClick={this.onFilter} color="primary" variant="contained" disabled={!Boolean(this.state.filterStr)}>
+            Filter
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
   toolbar = () => {
     return (
-      <div style={{ display: 'flex', width: '100%', position: 'fixed', top: 0, backgroundColor: '#fff' }}>
+      <div style={{ display: 'flex', width: '100%', position: 'fixed', top: 0, backgroundColor: '#fff', zIndex: 10 }}>
+        {this.filterWidget()}
 
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <Tooltip title='Open' placement='bottom'>
@@ -308,16 +447,24 @@ const onDragEnd = (e, state, setState, i) => {
   setState({ columns, dragX: null, dragI: null });
 }
 
+const colName = i => {
+  for (var ret = '', a = 1, b = 26; (i -= a) >= 0; a = b, b *= 26) {
+    ret = String.fromCharCode(parseInt((i % b) / a) + 65) + ret;
+  }
+  return ret;
+}
+
+const removeFilter = (i, state, setState, doFilter) => {
+  const { filters } = state;
+  delete filters[i];
+  setState({ filters });
+  doFilter(filters);
+}
+
 const SheetRenderer = props => {
   const { as: Tag, headerAs: Header, bodyAs: Body, rowAs: Row, cellAs: Cell,
-    className, columns, selections, onSelectAllChanged, classes, setState, state } = props
+    className, columns, selections, onSelectAllChanged, classes, setState, state, doFilter } = props
 
-  const colName = (i) => {
-    for (var ret = '', a = 1, b = 26; (i -= a) >= 0; a = b, b *= 26) {
-      ret = String.fromCharCode(parseInt((i % b) / a) + 65) + ret;
-    }
-    return ret;
-  }
 
   return (
     <div style={{ height: '100vh', cursor: state.dragX ? 'col-resize' : 'default' }} onMouseUp={(e) => onDragEnd(e, state, setState)}>
@@ -331,19 +478,30 @@ const SheetRenderer = props => {
             }
             {columns.map((column, i) => {
               const col = colName(i + 1);
+              const filtered = i in state.filters;
+
               return (
                 <Cell className='cell' className={classes.colCell} style={{ width: column.width, fontWeight: 600, backgroundColor: '#696', color: '#fff', padding: 0 }} key={column.id}>
-                  <div style={{ display: 'flex' }}>
-                    <div style={{
+                  <div style={{ display: 'flex', position: 'relative' }}>
+                    <span style={{
                       borderLeft: state.hoverCol === i - 1 || state.dragI === i - 1 ? '2px solid #8cdcda' : '0px solid #ccc',
-                      boxSizing: 'border-box', cursor: 'col-resize'
+                      boxSizing: 'border-box', cursor: 'col-resize', position: 'absolute', left: 0
                     }} onMouseOver={e => setState({ hoverCol: i - 1 })} onMouseLeave={e => setState({ hoverCol: null })} onMouseDown={(e) => { onDragStart(e, state, setState, i - 1) }} />
                     <span style={{ padding: '2px 0px', width: '100%', userSelect: 'none' }}>{col}</span>
                     <span style={{ flex: 1 }} />
-                    <div style={{
+                    <span style={{ position: 'absolute', right: 4 }}>
+                      <Tooltip title={filtered ? 'Remove Filter' : 'Add Filter'} placement='bottom'>
+                        <IconButton onClick={() => { filtered ? removeFilter(i, state, setState, doFilter) : setState({ filtering: true, filterInCol: i }) }} size='small' style={{ margin: '0px 4px' }}>
+                          <SvgIcon style={{ fontSize: 16 }}><path d={filtered ? mdiFilter : mdiFilterOutline} color={'#fff'} /></SvgIcon>
+                        </IconButton>
+                      </Tooltip>
+                    </span>
+                    <span style={{
                       borderRight: state.hoverCol === i || state.dragI === i ? '2px solid #8cdcda' : '1px solid #ccc',
-                      boxSizing: 'border-box', cursor: 'col-resize'
-                    }} onMouseOver={e => setState({ hoverCol: i })} onMouseLeave={e => setState({ hoverCol: null })} onMouseDown={(e) => { onDragStart(e, state, setState, i) }} />
+                      boxSizing: 'border-box', cursor: 'col-resize', position: 'absolute', right: 0, height: '100%', alignContent: 'center'
+                    }} onMouseOver={e => setState({ hoverCol: i })} onMouseLeave={e => setState({ hoverCol: null })} onMouseDown={(e) => { onDragStart(e, state, setState, i) }}>
+
+                    </span>
                   </div>
                 </Cell>
               );
@@ -374,7 +532,7 @@ const RowRenderer = props => {
   return (
     <Tag className={className}>
       <Cell className='action-cell cell' style={{ fontWeight: 600, textAlign: 'center', backgroundColor: '#696', color: '#fff', padding: '2px 0px' }}>
-        {row + 2 + (state.page * state.size)}
+        {props.cells.row ? props.cells.row + 2 : row + 2 + (state.page * state.size)}
       </Cell>
       {props.children}
     </Tag>
